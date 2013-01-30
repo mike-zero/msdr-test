1b14787ae76116fee65fe8632bb712e4ea5634a6
Mike Zero <m0@pisem.net>
2013-01-31 02:07:25 +0400

    test: stream_test.pl added

---
+++ rtl_sdr/stream_test.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my $filename = shift or 'aaa.bin';
+
+my $bufsize = 10240;
+my $buf = 0 x $bufsize;
+
+open F, $filename;
+binmode(F);
+while (sysread(F, $buf, $bufsize)) {
+	# syswrite(F, $buf, length($buf));
+	print ord(substr($buf,1,1)),"\n";
+}
+close F;
