2c07c368d92176a991e4ad6007763255949433f1
Mike Zero <m0@pisem.net>
2013-01-31 14:14:57 +0400

    stream_test.pl: print stat

--- rtl_sdr/stream_test.pl
+++ rtl_sdr/stream_test.pl
@@ -2,15 +2,23 @@
 
 use strict;
 
-my $filename = shift || 'aaa.bin';
+my $filename = shift || '-';
 
-my $bufsize = 10240;
+my $bufsize = 2 * 10240;
 my $buf = 0 x $bufsize;
+
+my %hh = ();
 
 open F, $filename;
 binmode(F);
 while (sysread(F, $buf, $bufsize)) {
 	# syswrite(F, $buf, length($buf));
-	print ord(substr($buf,1,1)),"\n";
+	for my $i (0..(length($buf)/2-1)) {
+		$hh{substr($buf, $i*2, 2)}++;
+	}
 }
 close F;
+foreach (sort { $hh{$b} <=> $hh{$a} } keys %hh) {
+	# printf("%X %X\t%d\n", unpack('C*', $_), $hh{$_});
+	printf("%d %d\t%d\n", unpack('c*', $_), $hh{$_});
+}
